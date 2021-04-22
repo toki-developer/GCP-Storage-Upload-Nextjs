386cdcefc68088d1e5147b4d6ac19aa90bea9ed1
Toki <55911995+tokio-k@users.noreply.github.com>
2021-04-22 17:40:10 +0900

    init

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import { ApolloProvider } from "@apollo/client";
+import { AppProps } from "next/dist/next-server/lib/router/router";
+import { apolloClient } from "../apollo/apolloClient";
+
+const App = (props: AppProps) => {
+    return (
+        <ApolloProvider client={apolloClient}>
+            <props.Component {...props.pageProps}/>
+        </ApolloProvider>
+    )
+}
+
+export default App;
